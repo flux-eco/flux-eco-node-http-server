9358b1f5da15fcbf30b50b12ec66de8dad170a6d
Martin Studer <martin@fluxlabs.ch>
2023-02-28 13:41:22 +0100

    Update StaticFileMiddleware.mjs

--- app/middlewares/StaticFileMiddleware.mjs
+++ app/middlewares/StaticFileMiddleware.mjs
@@ -1,28 +1,58 @@
+import fs from "node:fs";
+import path from "node:path";
+import stream from "node:stream";
+import url from "node:url";
+
+/** @typedef {import("node:http")} http */
+
 class StaticFileMiddleware {
+    /**
+     * @type {string}
+     */
+    #staticPath;
+
+    /**
+     * @param {string} staticPath
+     */
     constructor(staticPath) {
-        this.staticPath = staticPath;
+        this.#staticPath = staticPath;
     }
 
-    handleRequest(req, res, next) {
+    /**
+     * @param {http.IncomingMessage} req
+     * @param {http.ServerResponse} res
+     * @param {() => void} next
+     * @returns {Promise<void>}
+     */
+    async handleRequest(req, res, next) {
         const { pathname } = url.parse(req.url);
-        const sanitizedPathname = path.normalize(pathname).replace(/^(\.\.[/\\])+/, '');
-        const filePath = path.join(this.staticPath, sanitizedPathname);
 
-        fs.access(filePath, fs.constants.R_OK, (err) => {
-            if (err) {
-                // File not found, pass on to next middleware
-                next();
-                return;
-            }
+        if (pathname.includes("..") || pathname.includes("//") || path.includes("\\")) {
+            console.error(`Invalid path name: ${pathname}`);
+            next();
+            return;
+        }
 
-            const fileStream = fs.createReadStream(filePath);
-            pipeline(fileStream, res, (err) => {
-              if (err) {
-                console.error(`Error reading file: ${filePath}`, err);
+        const filePath = path.join(this.#staticPath, pathname);
+
+        try {
+            await fs.promises.access(filePath, fs.constants.R_OK);
+        } catch (err) {
+            console.error(`Error accessing file: ${filePath}`, err);
+            next();
+            return;
+        }
+
+        try {
+            await stream.promises.pipeline(fs.createReadStream(filePath), res);
+        } catch (err) {
+            console.error(`Error reading file: ${filePath}`, err);
+            if (!res.headersSent) {
                 res.statusCode = 500;
-                res.end();
-              }
-            });
-        });
+                res.statusMessage = "";
+            }
+        } finally {
+            res.end();
+        }
     }
 }
